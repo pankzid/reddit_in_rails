d8ac9536b990027368bfea51978c78c1c30e1f85
pankzid <pankzid@gmail.com>
2013-07-03 00:33:31 +0700

    Created reddit app in rails and add User model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedditInRails::Application.config.secret_token = '467cf6ce5bea62a4c6bd4030cd4ffd1d9974c40410ce4807067f23cc936d225f807837fe2e21f050f2b9b7cfe48594ffce69c942573a9ec01be1c32fe66dbe78'
---
+++ db/migrate/20130702172728_remove_name_in_user.rb
@@ -0,0 +1,6 @@
+class RemoveNameInUser < ActiveRecord::Migration
+  def change
+  	remove_column :users, :name
+  end
+
+end
